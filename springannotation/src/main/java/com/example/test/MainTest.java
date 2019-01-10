9665d4d03c9a9c7ad7c54f8f03cd5fef8b9766af
e <2029618609@qq.com>
2019-01-10 10:48:09 +0800

    传统bean的定义

--- springannotation/src/main/java/com/example/test/MainTest.java
+++ springannotation/src/main/java/com/example/test/MainTest.java
@@ -1,11 +1,16 @@
 package com.example.test;
 
+import com.example.vo.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainTest {
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("mybeans.xml");
-    applicationContext.get("person");
+    public static void main(String[] args) {
 
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("mybeans.xml");
+        Person person = (Person) applicationContext.getBean("person");
+        System.err.println(person);
+
+    }
 
 }
--- springannotation/src/main/java/com/example/vo/Person.java
+++ springannotation/src/main/java/com/example/vo/Person.java
@@ -1,11 +1,18 @@
 package com.example.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 
     private String name;
     private Integer age;
 
-    public Person() {
+/*    public Person() {
     }
 
     public Person(String name, Integer age) {
@@ -35,5 +42,5 @@ public class Person {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
-    }
+    }*/
 }
